d05b41f3db87e739a1d246797757504f5975aa77
kirangagitau <121950164+kirangagitau@users.noreply.github.com>
2023-01-20 11:23:14 -0500

    Change password of Root

--- Change _root _password.sh
+++ Change _root _password.sh
@@ -1,2 +1,7 @@
 !#/bin/bash
 #@Author Gitau Muguro
+# this script chnages password of root user incase forgoten.
+# NB: YOU HAVE TO HAVE ADMIN PRIVILEGES TO DO THAT.
+sudo su
+passwd
+# enter new root password and confirm it. done!
