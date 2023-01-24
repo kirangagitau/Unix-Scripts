2096bc56707db6cbc0a713b0656e6353c2c7a95a
kirangagitau <121950164+kirangagitau@users.noreply.github.com>
2023-01-24 19:36:49 +0300

    Update View_Date.sh

--- View_Date.sh
+++ View_Date.sh
@@ -7,3 +7,5 @@ cal #no options included
 #including opions to the cal command will show more details.
 # cal -y (show complete current year with current date highlighted)
 #cal 12 2023 (show calenda of selected month and year.)
+# cal -3 (show calenda of previous, current and next moth)
+# cal -j (show calenda in Julian calenda format rather than default gregorian calenda)
