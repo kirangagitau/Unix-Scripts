81de8b16618af43be279a8e6171ca60eaf0b82ec
kirangagitau <121950164+kirangagitau@users.noreply.github.com>
2023-01-24 19:47:40 +0300

    Time taken to run a command or program.

---
+++ Time_taken_by_Command_or_Program.sh
@@ -0,0 +1,10 @@
+!#/bin/bash/
+#@Author Gitau Muguro.
+
+#This command views the time taken to run  comman or program
+
+time ls -l
+
+# This runs the 'ls -l' command and shows content of the directory in question, followed by the time the 'ls -l' command has taken
+# appending 'time' command before and command or program raun from terminal displays the time taken by program/command after runninf it.
+# time ./a.out (will run the C the compilded program as well as show time for runnign the C compiled program)
