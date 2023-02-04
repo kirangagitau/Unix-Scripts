3bc92380b513fece885c6c4bcd493e19d562ad79
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-04 20:18:21 +0300

    Update Schedule_shutdown.sh

--- Schedule_shutdown.sh
+++ Schedule_shutdown.sh
@@ -5,6 +5,8 @@ echo '@Author Gitau Muguro'
 shutdown +300 "NICE TIME, GOOD PEOPLE"
 
 # 300 is in second, meaning shutdown system after 5 minute
-# The below command can be used to cancel the shceduled shutdown
+# Alternatively you can shedule a shutdown time i.e "sudo shutdown hh:mm" 
+# sudo shutdown 07:00 // thi will shutdown system at 7 AM  (24 HRS system)
 
+# The below command can be used to cancel the shceduled shutdown
 # sudo shutdown -c "Canceling The System Shutdown"
