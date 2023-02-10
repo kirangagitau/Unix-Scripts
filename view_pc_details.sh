3a8835c428bd8117aa5b74b1908ac5b014fd8482
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-10 20:40:56 +0300

    view info of PC

---
+++ view_pc_details.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ #command to show details of pc.
+ echo '@ author M Gitau.
+ 
+ uname -a
+ 
+ #will print infor such as kernel name, kernel version, kernel release,
+ #processor type, operating system, machine hardware name etc.
