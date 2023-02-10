48a949cc775cfc0846ace7d52130dfe424e77355
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-10 21:04:47 +0300

    view all network details.

---
+++ All_networks_Details.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This command show the information of the currently connected network with details such as
+#netmask, broadcastIP, ethernet, my ip among others.
+echo '@Author M. Gitau
+ifconfig -a
+# interface configuration details.
