1618abe5d4eef40ec415eb37108251b7b02819bc
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-24 03:57:08 -0800

    Shutdown Methods

---
+++ Amin_Operations/shutdown_command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+echo '@Author Gitau Muguro
+
+halt
+#Brings the system down immediately
+
+init 0
+#Powers off the system using predefined scripts to synchronize and clean up the system prior to shutting down
+
+init 6
+#Reboots the system by shutting it down completely and then restarting it
