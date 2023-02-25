5ea3fef81601075d23c19c92541481a4f11688a8
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-25 11:47:32 -0800

    View loggedin and logged out users
    The last command in Linux is used to display the list of all the users logged in and out since the file /var/log/wtmp was created. One or more usernames can be given as an argument to display their login in (and out) time and their host-name.

---
+++ Amin_Operations/The_last_command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo '@Author Gitau Muguro.'
+
+# The un-commented command of this script displays the user login and out of system including the dates.
+
+last -F
+
+# Basic syntax of 'last' command is.
+# last [options] [username...] [tty...]
+
+#last --help  :Gives the help of the last command.
