95f0ed70f79a62ec363d2cd11ca84cbc2d4b743c
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-25 11:29:19 -0800

    The 'ps' command
    See your own processes by running the ps (process status) command

---
+++ Amin_Operations/The_ps_command.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+echo '@Author Gitau Muguro.'
+
+ps -f 
+
+#Other options of the p.s command
+# -a :Shows information about all users
+# -x :Shows information about processes without terminals
+# -u :Shows additional information like -f option
+# -e :Displays extended information
